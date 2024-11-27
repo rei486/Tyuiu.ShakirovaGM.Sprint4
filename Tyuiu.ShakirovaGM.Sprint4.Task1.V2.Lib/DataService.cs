88f417958ff4e10ecdbc162aa98ac43339f01171
tyuiu.ShakirovaGM@outlook.com <tyuiu.ShakirovaGM@outlook.com>
2024-11-27 06:21:10 +0500

    Task1

--- Tyuiu.ShakirovaGM.Sprint4.Task1.V2.Lib/DataService.cs
+++ Tyuiu.ShakirovaGM.Sprint4.Task1.V2.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ShakirovaGM.Sprint4.Task1.V2.Lib
         public int Calculate(int[] array)
         {
             int sum = 0;
-            for (int i = 1; i <=7; i++)
+            for (int i = 0; i <=array.Length-1; i++)
             {
                 if (array[i] % 2 != 0)
                     sum += array[i];
--- Tyuiu.ShakirovaGM.Sprint4.Task1.V2.Test/DataServiceTest.cs
+++ Tyuiu.ShakirovaGM.Sprint4.Task1.V2.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.ShakirovaGM.Sprint4.Task1.V2.Test
             DataService ds = new DataService();
             int[] numsArray = { 1, 7, 2, 2, 6, 2, 3, 2, 3, 5, 7, 7, 1, 2 };
             int res = ds.Calculate(numsArray);
-            int wait = 10;
+            int wait = 34;
             Assert.AreEqual(res, wait);
         }
     }
