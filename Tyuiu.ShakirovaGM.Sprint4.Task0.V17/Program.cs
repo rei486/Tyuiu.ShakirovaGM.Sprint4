27b87c9146fe04040674a77ec16f4ae3b01f0e2a
tyuiu.ShakirovaGM@outlook.com <tyuiu.ShakirovaGM@outlook.com>
2024-11-27 05:45:54 +0500

    Task0

---
+++ Tyuiu.ShakirovaGM.Sprint4.Task0.V17/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.ShakirovaGM.Sprint4.Task0.V17.Lib;
+namespace Tyuiu.ShakirovaGM.Sprint4.Task0.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Шакирова Г. М. | РППб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Шакирова Гузель Мансуровна | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать сумму четных   *");
+            Console.WriteLine("* элементов массива.  {4 ,8 ,7 ,6 ,5 ,8 ,2 ,4 ,3 ,2}                      *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] numsArray = { 4, 8, 7, 6, 5, 8, 2, 4, 3, 2 };
+
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i <= numsArray.Length - 1; i++)
+            {
+               Console.WriteLine(numsArray[i]);
+            }
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма четных элеменетов массива = " + ds.GetSumEvenArrEl(numsArray));
+
+            Console.ReadLine();
+        }
+    }
+}
