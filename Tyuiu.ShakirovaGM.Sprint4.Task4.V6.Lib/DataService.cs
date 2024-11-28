0eefe01e893d656406a3cdd9d13de8425b28c16f
tyuiu.ShakirovaGM@outlook.com <tyuiu.ShakirovaGM@outlook.com>
2024-11-29 01:07:11 +0500

    Task4

---
+++ Tyuiu.ShakirovaGM.Sprint4.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.ShakirovaGM.Sprint4.Task4.V6.Lib
+{
+    public class DataService : ISprint4Task4V6
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int cols = matrix.Length / rows;
+            
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] % 2 != 0) 
+                    {
+                        matrix[i, j] = 0;
+                    }
+                   
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.ShakirovaGM.Sprint4.Task4.V6/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.ShakirovaGM.Sprint4.Task4.V6.Lib;
+namespace Tyuiu.ShakirovaGM.Sprint4.Task4.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
